e9f4d9678a72646d6739b5811be97eb56fec32d7
waggo7 <63073660+waggo7@users.noreply.github.com>
2020-06-17 00:45:07 -0500

    Add files via upload
    added css after functionality was established

--- memegen.js
+++ memegen.js
@@ -6,26 +6,22 @@ document.addEventListener("DOMContentLoaded", function() {
     var topText = document.getElementById("toptext");
     var btmText = document.getElementById("btmtext");
     var container = document.getElementById("container");
-
+    var table = document.getElementById("table")
     submit();
 
     function submit() {
         let count = 0;
         memeimage.addEventListener("submit", function(e) {
             e.preventDefault()
-            createCanvas();
             count++;
-            console.log(count);
-            //if (count > 1) {
-            // var meme2 = document.createElement("tr")
-            // var canvas2 = document.createElement("canvas")
-            // container.append("meme2")
-            // meme2.append("canvas2")
+            createCanvas();
+            alertMeme();
 
-            // }
+
         })
 
         function createCanvas() {
+
             var canvas = document.createElement('canvas')
             var ctx = canvas.getContext("2d")
             var placement = document.createElement("td")
@@ -33,46 +29,53 @@ document.addEventListener("DOMContentLoaded", function() {
             var image = document.createElement("img");
             canvas.id = "canvas";
             image.height = canvas.height
-            image.width = canvas.width
-                // canvas.style.height = "400"
-                // canvas.style.width = "400"
-            image.class = "image";
-            //image.id = "image";
+            image.width = canvas.width;
             image.src = input.value;
+            image.opacity = .2;
             container.append(placement);
             placement.append(canvas);
+
             image.addEventListener("load", function() {
-                ctx.drawImage(image, 0, 0, image.width, image.height)
+                ctx.font = "30px small-caps impact"
+                ctx.fillStyle = "White"
+                ctx.textAlign = "centered"
 
+                ctx.drawImage(image, 0, 0, image.width, image.height);
                 ctx.fillText(topText.value, 40, 50)
-                ctx.fillText(btmText.value, 40, 150)
+                ctx.fillText(btmText.value, 40, 120)
             })
-            ctx.font = "30px serif"
-            ctx.fillStyle = "White"
-            ctx.textAlign = "centered";
-            // Buttons();
 
-            // function Buttons() {
-            //     let anchor = document.createElement("a")
-            //     canvas.prepend(anchor)
-            //     anchor.id = "anchor"
-            //     anchor.href = 'meme.png'
-            //     anchor.download = canvas;
-            //     anchor.addEventListener("click", function(e) {
-            //         let urlmeme = canvas.toDataURL("image/png")
-            //         localStorage.set("canvas", urlmeme);
-            //     })
-            const button = document.createElement("button");
-            button.id = "button"
-            button.innerText = "X"
-            placement.append(button)
-            button.addEventListener("click", function(e) {
-                canvas.parentNode.removeChild(canvas);
-                button.style.display = "none"
-            })
+            deleteButton();
+            //DO NOT CHANGE ANYTHING BELOW THIS!!!
+            function deleteButton() {
+                const button = document.createElement("button");
+                button.id = "button"
+                button.innerText = "X"
+                container.append(button)
+                button.addEventListener("click", function(e) {
+                    if (button) {
+                        canvas.parentNode.removeChild(canvas);
+                        button.style.display = "none";
+                    }
+                })
+            }
 
         }
 
+        function alertMeme() {
+            if (count >= 1) {
+                alert("next viral meme coming your way!!");
+            }
+        }
     }
 
 })
+
+
+
+function download() {
+    var imagedwnld = new Image();
+    imagedwnld.src = ctx;
+    localStorage(imagedwnld)
+
+}
